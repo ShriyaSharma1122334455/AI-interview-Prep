a3d879e763549c80aefed1761523fcae5ca37285
Shri <ss5379@njit.edu>
2025-08-20 05:08:56 -0400

    desinged the interview session

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,13 @@
 import Agents from '@/components/Agents';
+import { getCurrentUser } from '@/lib/actions/auth.action';
 import React from 'react';
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
   return (
     <>
       <h3>Interview Generator</h3>
-      <Agents userName="You" userId="user1" type="generate" />
+      <Agents userName={user?.name} userId={user?.id} type="generate" />
     </>
   );
 };
